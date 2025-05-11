b65aabd8916e002dfaa6000f92841a6196cdadac
David Latorre <david@latorredev.com>
2025-05-11 01:58:30 -0500

    fix/fix domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bloglatorredev.github.io',
+  base: "/blog/",
   vite: {
     plugins: [tailwindcss()],
   },
